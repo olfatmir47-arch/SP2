a86c987047e61ac99412c232079164d640e23022
olfatmir47-arch <olfatmir47@gmail.com>
2025-10-24 10:32:10 +0200

    rettet

--- src/bibliotekspenge/LibraryRoyaltyCalculator.java
+++ src/bibliotekspenge/LibraryRoyaltyCalculator.java
@@ -3,21 +3,23 @@ package bibliotekspenge;
 public class LibraryRoyaltyCalculator {
     public static void main(String[] args) {
 
+        //opretter forfatter
         Author newAuthor = new Author("Karen Blixen");
         Author newAuthor1 = new Author("H.C. Andersen");
 
+        //opretter bøger
         AudioBook aBook1 = new AudioBook("Engle og Dæmoner", "TE", 132, 180);
         PrintedBook pBook1 = new PrintedBook("Engle og Dæmoner", "TE", 150, 333);
         PrintedBook pBook2 = new PrintedBook("Rip, Rap og Rup", "SKØN", 999, 55);
-
         PrintedBook pBook3 = new PrintedBook("Fyrtøjet", "LYRIK", 10000, 120);
 
+        //tilføjer bøger til forfatter
         newAuthor.addTitle(aBook1);
         newAuthor.addTitle(pBook1);
         newAuthor.addTitle(pBook2);
         newAuthor1.addTitle(pBook3);
 
-        newAuthor.calculateRoyalties();
+        //printer points ud
         System.out.println("forfatter: " + newAuthor.getName() + " :" + newAuthor.calculateRoyalties() + "dkk");
 
         System.out.println("forfatter: " + newAuthor1.getName() + " :" + newAuthor1.calculateRoyalties() + "dkk");
